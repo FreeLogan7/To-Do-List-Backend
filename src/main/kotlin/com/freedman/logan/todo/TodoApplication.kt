062c3b2bd459b79fa454f95389bd0f8c16b14b65
freelogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-09-21 13:42:51 -0400

    Setting up todo list backend

---
+++ src/main/kotlin/com/freedman/logan/todo/TodoApplication.kt
@@ -0,0 +1,11 @@
+package com.freedman.logan.todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoApplication>(*args)
+}
